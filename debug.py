5a6ee1b5f02153863c2c76f34e722ba3ed5656c2
LISN 2022 <maxime.criado@universite-paris-saclay.fr>
2022-05-30 15:00:13 +0200

    maj

--- debug.py
+++ debug.py
@@ -4,30 +4,24 @@ import triage as t
 import display as d
 
 
-CompleteList = t.triage("new_analysis_nf")
+CompleteList = t.triage("new_new_analysis_nf")
 nb_graph = 0
 
 #print(CompleteList)
 for y in CompleteList:
     
     
-    if nb_graph < 597 : ## a enlever plus tard juste pour les test
-        for path, subdirs, files in os.walk(y):
-            
-            #print(nb_graph, path)
-            for name in files:
-                if name == "structure_worklow" :             
-                    workflow = os.path.join(path, name)
+    if nb_graph < 5 : ## a enlever plus tard juste pour les test
+        
+        print(y)   
+        id_sommet = p.new_new_Parsing(y)[0]
+        #print(id_sommet)
+        partenaire = p.new_new_Parsing(y)[1]
+        #print(partenaire)
 
-                elif name == "processes_info.json" :
-                    json = os.path.join(path, name)
-            #print(nb_graph, path) ## pb avec le fichier 75 ....
-            
-            id_sommet = p.Traitement_Structure(workflow)[0]
-            partenaire = p.Traitement_Structure(workflow)[1]
 
-            #print(partenaire)
-            d.Affichage(id_sommet, partenaire, nb_graph)
-            nb_graph += 1
+        #print(partenaire)
+        d.Affichage(id_sommet, partenaire, nb_graph)
+        nb_graph += 1
             
         
