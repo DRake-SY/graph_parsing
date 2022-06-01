985fd1a5dc9f3d779193b0f3f4a6ef7ab618bd2c
LISN 2022 <maxime.criado@universite-paris-saclay.fr>
2022-06-01 06:57:27 +0200

    maj

--- main.py
+++ main.py
@@ -7,16 +7,14 @@ import display as d
 CompleteList = t.triage("new_new_analysis_nf")
 nb_graph = 0
 
-#print(CompleteList)
+
 for y in CompleteList:
       
-    id_sommet = p.new_new_Parsing(y)[0]
-        #print(id_sommet)
-    partenaire = p.new_new_Parsing(y)[1]
-        #print(partenaire)
+    res =  p.new_new_Parsing(y)
 
-
-        #print(partenaire)
+    id_sommet = res[0] 
+    partenaire = res[1]
+        
     d.Affichage(id_sommet, partenaire, nb_graph)
     nb_graph += 1
             
